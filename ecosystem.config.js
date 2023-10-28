db9c4984139961fd48a40e26479a7f78c69b5264
Ditotisi Rasyid Sumpena <ditolord@gmail.com>
2023-10-28 08:44:03 +0700

    add: pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "Image Service",
+      script: "dist/server.js", // The entry point of your application
+      instances: 1, // Number of instances to run (adjust as needed)
+      autorestart: true,
+      watch: false, // Set to true if you want to enable file watching
+      max_memory_restart: "500M", // Restart if memory usage exceeds this
+      env: {
+        NODE_ENV: "production", // Adjust as needed
+        PORT: 4000, // Specify your desired port
+      },
+      env_development: {
+        NODE_ENV: "development",
+        PORT: 4000,
+      },
+    },
+  ],
+};
