6c52d9bce0db45971d6b82b250f6453cc6f328c2
Ditotisi Rasyid Sumpena <ditolord@gmail.com>
2024-01-14 15:35:23 +0700

    add image quality params

--- controller/image-controller.ts
+++ controller/image-controller.ts
@@ -5,28 +5,23 @@ import sharp from "sharp";
 class ImageController {
   static get(req: Request, res: Response) {
     const temp = path.join(__dirname + "/../../", "public/static/img/", req.params.filename);
-    // const imagePath = path.join(__dirname, "public/static/img", req.params.filename);
     const imagePath = temp;
-    // res.send(temp);
     const width = Number(req.query.width) || null;
     const height = Number(req.query.height) || null;
-
-    if (width || height) {
-      const imageFormat = path.extname(imagePath).substr(1);
-      sharp(imagePath)
-        .resize(width, height)
-        .toBuffer()
-        .then((data) => {
-          res.setHeader("Content-Type", `image/${imageFormat}`);
-          res.end(data);
-        })
-        .catch((err) => {
-          console.error(err);
-          res.status(500).send("Error resizing image");
-        });
-    } else {
-      res.sendFile(imagePath);
-    }
+    const quality = Number(req.query.quality ?? 100);
+    const imageFormat = path.extname(imagePath).substr(1);
+    sharp(imagePath)
+      .resize(width, height)
+      .webp({ quality })
+      .toBuffer()
+      .then((data) => {
+        res.setHeader("Content-Type", `image/${imageFormat}`);
+        res.end(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error resizing image");
+      });
   }
 }
 
